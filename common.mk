00613ce86471d812204fdacf5d1ca87053511ee5
root <@sunchness.com>
2018-03-15 17:35:51 +0800

    add link_obj can not run

--- common.mk
+++ common.mk
@@ -3,13 +3,20 @@
 SRCS = $(wildcard *.c)
 OBJS = $(SRCS:.c=.o)
 DEPS = $(SRCS:.c=.d)
+
+LINK_OBJ_DIR =/home/xie/work/cppTest/makeFileTest/app/link_obj
+$(shell mkdir -p $(LINK_OBJ_DIR))
+
+OBJS := $(addprefix $(LINK_OBJ_DIR)/,$(OBJS))
+LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
+
 all:$(BIN) $(DEPS) $(OBJS)
 ifneq ("$(wildcard $(DEPS))","")
 include $(DEPS)
 endif
-$(BIN):$(OBJS)
-	gcc -o $@ $^ ../lcd/lcd.o ../usb/usb.o ../media/media.o
-%.o:%.c
+$(BIN):$(LINK_OBJ)
+	gcc -o $@ $^ $(OBJS)
+$(LINK_OBJ_DIR)/%.o:%.c
 	gcc -o $@ -c $(filter %.c,$^)
 %.d:%.c
 	gcc -MM $^ > $@
