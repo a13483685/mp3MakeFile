b0097d5174bd3fc84182028a95f7d65f434e8404
root <@sunchness.com>
2018-03-24 15:12:19 +0800

    变量可以使用export来进行传递

--- common.mk
+++ common.mk
@@ -5,9 +5,9 @@ OBJS = $(SRCS:.c=.o)
 DEPS = $(SRCS:.c=.d)
 BIN  := $(addprefix /home/xie/work/cppTest/makeFileTest/,$(BIN)) 
 
-LINK_OBJ_DIR =/home/xie/work/cppTest/makeFileTest/app/link_obj
+LINK_OBJ_DIR =$(BUILD_ROOT)/app/link_obj
 $(shell mkdir -p $(LINK_OBJ_DIR))
-DEPS_DIR = /home/xie/work/cppTest/makeFileTest/app/dep
+DEPS_DIR = $(BUILD_ROOT)/app/dep
 $(shell mkdir -p $(DEPS_DIR))
 
 OBJS := $(addprefix $(LINK_OBJ_DIR)/,$(OBJS))
@@ -25,6 +25,6 @@ $(BIN):$(LINK_OBJ)
 $(LINK_OBJ_DIR)/%.o:%.c
 	gcc -o $@ -c $(filter %.c,$^)
 $(DEPS_DIR)/%.d:%.c
-	gcc -MM $^ | sed 's,\(.*\).o[ :]*,$(LINK_OBJ_DIR)/\1.o $@:,g' > $@
+	gcc -MM $(filter %.c,$^) | sed 's,\(.*\).o[ :]*,$(LINK_OBJ_DIR)/\1.o $@:,g' > $@
 clean:
 	rm -f $(BIN) $(OBJS) $(DEPS)
